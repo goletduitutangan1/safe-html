bca3b8ba2328d4b411158e9ed15aed335377e577
goletduitutangan1 <goletduitutangan@gmail.com>
2017-06-01 19:29:26 +0700

    Add files via upload

--- Javascript-Multy.js
+++ Javascript-Multy.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="https://safekabeh.blogspot.com/2017/06/text-graphic-on-adsense.html"}
-if(x=="2"){xxx="https://safekabeh.blogspot.com/2017/06/the-disadvantages-of-adsense.html"}
-if(x=="3"){xxx="https://safekabeh.blogspot.com/2017/06/the-future-of-google-adsense.html"}
-if(x=="4"){xxx="https://safekabeh.blogspot.com/2017/06/where-adsense-should-appear.html"}
-if(x=="5"){xxx="https://safekabeh.blogspot.com/2017/06/why-use-google-adsense.html"}a_to_ve[i].href="https://safekabeh.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="https://safekabeh.blogspot.com/text-graphic-on-adsense.html"}
+if(x=="2"){xxx="https://safekabeh.blogspot.com/the-disadvantages-of-adsense.html"}
+if(x=="3"){xxx="https://safekabeh.blogspot.com/the-future-of-google-adsense.html"}
+if(x=="4"){xxx="https://safekabeh.blogspot.com/where-adsense-should-appear.html"}
+if(x=="5"){xxx="https://safekabeh.blogspot.com/why-use-google-adsense.html"}a_to_ve[i].href="https://safekabeh.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
